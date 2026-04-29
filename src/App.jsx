707326150b9dd47f623f1680dabc9ba4516c7411
seunghee <daalgwabyul@naver.com>
2026-04-29 12:48:00 +0900

    Feat: 회원가입 단계 화면 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 const App = () => {
+  const [page, setPage] = React.useState('login')
+
+  const pages = {
+    login: (
+      <Login
+        onSignupClick={() => setPage('signup')}
+        onResetPasswordClick={() => setPage('reset')}
+      />
+    ),
+    signup: <Signup onLoginClick={() => setPage('login')} />,
+  }
+
   return (
     <div className='container auth-container'>
-      <Login />
+      {pages[page] || pages.login}
     </div>
   )
 }
