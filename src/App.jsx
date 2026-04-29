d05232a8461e9e50b9f03e29e9777db3a327fd90
seunghee <daalgwabyul@naver.com>
2026-04-29 12:49:01 +0900

    Feat: 인증 화면 흐름 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './pages/Login'
 import PasswordReset from './pages/PasswordReset'
 import Signup from './pages/Signup'
+import Splash from './pages/Splash'
 
 const App = () => {
-  const [page, setPage] = React.useState('login')
+  const [page, setPage] = useState('splash')
+
+  useEffect(() => {
+    if (page !== 'splash') {
+      return undefined
+    }
+
+    const timer = window.setTimeout(() => {
+      setPage('login')
+    }, 900)
+
+    return () => window.clearTimeout(timer)
+  }, [page])
 
   const pages = {
+    splash: <Splash />,
     login: (
       <Login
         onSignupClick={() => setPage('signup')}
