be77a171ad3f941633ce3ef9eb62d7b8986c9609
kimseongyeon <seongyeon0810@naver.com>
2026-05-01 01:33:06 +0900

    Feat: OnboardingIngo 페이지 제작 및 수강중인 과목 추가, 삭제 로직 구현

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Input = ({ title }) => {
+const Input = ({ title, onChange, value }) => {
   return (
     <div className="Input_Wrap">
       <div className="title">{title}</div>
-        <input type="text" />
+        <input type="text" 
+        placeholder='내용을 입력해주세요.' 
+        onChange={onChange}
+        value={value}
+        />
     </div>
   )
 }
--- src/pages/Onboarding/OnboardingInfo.jsx
+++ src/pages/Onboarding/OnboardingInfo.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Input from '../../components/Input'
 import Dropdown from '../../components/Dropdown'
+import Button from '../../components/Button'
+import { useState } from 'react'
+import delete_img from '../../assets/images/Onboarding/delete.svg'
 
 const OnboardingInfo = () => {
 
   const major = ["컴퓨터공학과", "서비스디자인공학과"];
   const grade = [1,2,3,4];
   const semester= ["2026년 1학기", "2025년 2학기", "2025년 1학기", "2024년 2학기", "2024년 1학기"];
+
+  const [lectureList, SetLectureList] = useState([])
+  const [input, SetInput] = useState("")
+
+  const handleAddLecture = () => {
+    if(input.trim() === "")
+      return
+    SetLectureList([...lectureList, input])
+    SetInput("")
+  }
+
+  const deleteLecture = (index) => {
+    const newList = lectureList.filter((_, i) => i !== index);
+    SetLectureList(newList)
+  }
 
   return (
     <div id="OnboardingInfo_Wrap" className="container">
@@ -15,6 +33,25 @@ const OnboardingInfo = () => {
         <Dropdown title={"전공"} list={major} />
         <Dropdown title={"학년"} list={grade} />
         <Dropdown title={"학기"} list={semester} />
+        <div className="lecture_container">
+          <div className="text_container">
+            <h1>수강중인 강의</h1>
+            <p>*추후 수정 및 변경이 가능합니다.</p>
+          </div>
+          <div className="input_container">
+            <Input value={input} onChange={((e) => SetInput(e.target.value))} />
+            <button className="add" onClick={() => handleAddLecture()} >등록</button>
+          </div>
+          <div className="lecture_content">
+            {lectureList.map((item, index) => ( 
+              <div className="lecture" key={index}>
+                <p>{item}</p>
+                <img src={delete_img} alt="삭제" onClick={() => deleteLecture(index)} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <Button title={"다음"}/>
     </div>
   )
 }
