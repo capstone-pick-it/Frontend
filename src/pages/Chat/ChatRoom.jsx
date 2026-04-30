8e69fadbb8fc9c7603c3dec882b7d19f710e7fb6
kimseongyeon <seongyeon0810@naver.com>
2026-05-01 01:34:56 +0900

    Chore: 미사용 이미지 파일 제거

--- src/pages/Chat/ChatRoom.jsx
+++ src/pages/Chat/ChatRoom.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import team_request from '../../assets/images/Chat/user-add.svg'
-import team_accept from '../../assets/images/Chat/user-tick.svg'
 import ChatMessage from '../../components/Chat/ChatMessage'
 import { useParams } from 'react-router-dom'
 import { CHAT_MESSAGES } from '../../data/mockData'
