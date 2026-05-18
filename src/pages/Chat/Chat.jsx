255fcf4abf3d0fc2a9c76adebd28a93529543295
kimseongyeon <seongyeon0810@naver.com>
2026-05-18 15:13:07 +0900

    Refactor: Chat 페이지 코드 수정

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
 import Nav from '../../components/Nav'
 import ChatList from '../../components/Chat/ChatList'
-import ChatRoom from './ChatRoom'
 import TopBar from '../../components/TopBar'
 import GroupChatList from '../../components/Chat/GroupChatList'
 
 const Chat = () => {
-    const { roomId } = useParams();
   return (
     <div id="Chat_Wrap" className="container">
        <TopBar title={"채팅"}/>
         <ChatList/>
         <GroupChatList/>
-        {roomId && <ChatRoom/>} 
         <Nav/>
     </div>
   )
